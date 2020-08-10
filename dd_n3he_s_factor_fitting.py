3bbe8ce125edaddc9c61d39f5c03b1bcb62c00f2
peterallangk <phy3pa@gmail.com>
2020-08-10 17:02:55 +0100

    Tweaking fit

--- dd_n3he_s_factor_fitting.py
+++ dd_n3he_s_factor_fitting.py
@@ -35,18 +35,22 @@ def bayesian_fit(x, y, y_error, num_samples=500):
         A2 = pm.Flat("A2")
         A3 = pm.Flat("A3")
         A4 = pm.Flat("A4")
-        y_stdev = pm.HalfNormal("Y_stdev", sd=1)
+        B1 = pm.Flat("B1")
+        B2 = pm.Flat("B2")
+        B3 = pm.Flat("B3")
+        B4 = pm.Flat("B4")
+        # y_stdev = pm.HalfNormal("Y_stdev", sd=1)
 
-        y_mean = pm.Deterministic("y_mean", pade_func(x, A1, A2, A3, A4))
+        y_mean = pm.Deterministic("y_mean", pade_func(x, A1, A2, A3, A4, B1, B2, B3, B4))
         # y_pred = pm.Normal("y_pred", mu=y_mean, observed=y)
-        y_pred = pm.Normal("y_pred", mu=y_mean, sd=y_error, observed=y)
-        # y_pred = pm.Normal("y_pred", mu=y_mean, tau=1.0/y_error**2, observed=y)
+        # y_pred = pm.Normal("y_pred", mu=y_mean, sd=y_error, observed=y)
+        y_pred = pm.Normal("y_pred", mu=y_mean, tau=1.0/y_error**2, observed=y)
 
         # Do the fitting
         start = pm.find_MAP()
         trace = pm.sample(num_samples, start=start, tune=int(num_samples / 2.0), chains=mp.cpu_count())
 
-    varnames = ["A1", "A2", "A3", "A4"]
+    varnames = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4"]
     pm.traceplot(trace, var_names=varnames)
     summary = az.summary(trace, var_names=varnames, credible_interval=0.95)
     print(summary)
@@ -77,16 +81,16 @@ def run():
                 marker="o", color="b", elinewidth=1, linewidth=0, markersize=2, zorder=0)
 
     # Least-squares fit
-    coefs = lsq_fit(pade_func, energy, s_factor)
-    energy_fit = np.arange(0.8 * min(energy), 2 * max(energy), 1)
-    s_factor_fit = pade_func(energy_fit, *coefs)
-    plt.plot(energy_fit, s_factor_fit, "r-")
+    # coefs = lsq_fit(pade_func, energy, s_factor)
+    # energy_fit = np.arange(0.8 * min(energy), 2 * max(energy), 1)
+    # s_factor_fit = pade_func(energy_fit, *coefs)
+    # plt.plot(energy_fit, s_factor_fit, "r-", label="lsqs")
 
     # Do Bayesian fitting
-    # y_fit, y_fit_min, y_fit_max = bayesian_fit(energy, s_factor, s_factor_error, num_samples=2000)
-    # plt.plot(energy, y_fit, "r-", lw=2)
-    # plt.plot(energy, y_fit_min, "r--", lw=1)
-    # plt.plot(energy, y_fit_max, "r--", lw=1)
+    y_fit, y_fit_min, y_fit_max = bayesian_fit(energy, s_factor, s_factor_error, num_samples=200)
+    plt.plot(energy, y_fit, "g-", lw=2, label="PyMC3")
+    plt.plot(energy, y_fit_min, "r--", lw=1)
+    plt.plot(energy, y_fit_max, "r--", lw=1)
 
     ax.set_xlim(1, 2e4)
     ax.set_ylim(0.025, 4)
